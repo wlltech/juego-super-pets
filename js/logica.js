844ae12c93659ec446100529ed3064219dc9891e
wilmanvargas <wilman.vargas2@vmlyr.com>
2023-10-04 21:37:21 -0500

    Agregando etiqueta script

---
+++ js/logica.js
@@ -0,0 +1,6 @@
+function seleccionarMascotaJugador(){
+    alert('Mascota seleccionada');
+}
+
+let botonSeleccionarMascota = document.getElementById('boton-seleccionar-mascota');
+botonSeleccionarMascota.addEventListener('click', seleccionarMascotaJugador);
