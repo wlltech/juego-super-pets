5a831de22f6c82459455720f13999d03310cfed3
wilmanvargas <wilman.vargas2@vmlyr.com>
2023-10-16 20:00:50 -0500

    Añadiendo addEventListener al botón de seleccionar mascota

--- js/logica.js
+++ js/logica.js
@@ -1,6 +1,21 @@
-function seleccionarMascotaJugador(){
-    alert('Mascota seleccionada');
+function iniciarJuego(){
+    let botonSeleccionarMascota = document.getElementById('boton-seleccionar-mascota');
+    botonSeleccionarMascota.addEventListener('click', seleccionarMascotaJugador);
 }
 
-let botonSeleccionarMascota = document.getElementById('boton-seleccionar-mascota');
-botonSeleccionarMascota.addEventListener('click', seleccionarMascotaJugador);
+function seleccionarMascotaJugador(){
+    let inputPerro = document.getElementById('perro');
+    let inputGato = document.getElementById('gato');
+    let inputCaracol = document.getElementById('caracol');
+
+    if (inputPerro.checked){
+        alert("Seleccionaste el Perro")
+    } else if (inputGato.checked){
+        alert("Seleccionaste el Gato")
+    } else if (inputCaracol.checked){
+        alert("Seleccionaste el Caracol")
+    }
+}
+
+
+window.addEventListener('load', iniciarJuego);
